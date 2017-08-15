b6f199037a923c213bb989915ec09b48bb526740
K1K0 <kiko.goinhas98@gmail.com>
2017-08-15 17:39:53 +0100

    Fix Layouts for ChooseSubjectsActivity

--- ibstudyplanner/app/src/main/java/com/example/kiko/ibstudentplannerapp/ChooseSubjectsActivity.java
+++ ibstudyplanner/app/src/main/java/com/example/kiko/ibstudentplannerapp/ChooseSubjectsActivity.java
@@ -2,6 +2,14 @@ package com.example.kiko.ibstudentplannerapp;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -16,22 +24,43 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.security.auth.Subject;
 
 public class ChooseSubjectsActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private LinearLayout mActivityLayout;
+    private View mContentView;
+
+    private EditText mSubject1EditText;
+    private EditText mSubject2EditText;
+    private EditText mSubject3EditText;
+    private EditText mSubject4EditText;
+    private EditText mSubject5EditText;
+    private EditText mSubject6EditText;
+
+    private RadioGroup mSubject1RadioGroup;
+    private RadioGroup mSubject2RadioGroup;
+    private RadioGroup mSubject3RadioGroup;
+    private RadioGroup mSubject4RadioGroup;
+    private RadioGroup mSubject5RadioGroup;
+    private RadioGroup mSubject6RadioGroup;
+
+    private Button mAcceptButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_subjects);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mActivityLayout = (LinearLayout) findViewById(R.id.activity_chooser_layout);
-        createSubjectChooser(mActivityLayout);
+        createSubjectChooser();
+        mContentView = findViewById(R.id.activity_chooser_layout);
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -45,49 +74,57 @@ public class ChooseSubjectsActivity extends AppCompatActivity implements View.On
         return super.onOptionsItemSelected(item);
     }
 
-    private void createSubjectChooser(ViewGroup viewGroup) {
+    private void createSubjectChooser() {
 
-        for (int i = 0; i < 6; i++){
-            int subjectGroupNumber = i+1;
-            View view = (LinearLayout) getLayoutInflater().inflate(R.layout.subject_row_chooser, viewGroup, true);
-            LinearLayout viewLayout = (LinearLayout) view;
-            if (((LinearLayout) view).getChildAt(i) instanceof LinearLayout) {
+        mSubject1EditText = (EditText) findViewById(R.id.et_subject_1);
+        mSubject2EditText = (EditText) findViewById(R.id.et_subject_2);
+        mSubject3EditText = (EditText) findViewById(R.id.et_subject_3);
+        mSubject4EditText = (EditText) findViewById(R.id.et_subject_4);
+        mSubject5EditText = (EditText) findViewById(R.id.et_subject_5);
+        mSubject6EditText = (EditText) findViewById(R.id.et_subject_6);
 
-                LinearLayout layout = (LinearLayout) viewLayout.getChildAt(i);
-                EditText editText = (EditText) layout.getChildAt(0);
-                editText.setHint("Group " + subjectGroupNumber + " Subject");
-            }
+        mSubject1RadioGroup = (RadioGroup) findViewById(R.id.rg_subject_level_1);
+        mSubject2RadioGroup = (RadioGroup) findViewById(R.id.rg_subject_level_2);
+        mSubject3RadioGroup = (RadioGroup) findViewById(R.id.rg_subject_level_3);
+        mSubject4RadioGroup = (RadioGroup) findViewById(R.id.rg_subject_level_4);
+        mSubject5RadioGroup = (RadioGroup) findViewById(R.id.rg_subject_level_5);
+        mSubject6RadioGroup = (RadioGroup) findViewById(R.id.rg_subject_level_6);
 
-        }
+        mAcceptButton = (Button) findViewById(R.id.bt_accept_button);
+        mAcceptButton.setOnClickListener(this);
 
-        Button button = new Button(this);
-        button.setText("Accept");
-        button.setOnClickListener(this);
-        viewGroup.addView(button);
+
     }
 
-    private IBSubject[] getSubjectData(){
-        IBSubject[] subjectsArray = new IBSubject[6];
-
+    private ArrayList<IBSubject> getSubjectData(){
+        ArrayList<IBSubject> subjectsArray = new ArrayList<IBSubject>();
         for (int i = 0; i < 6; i++){
-            View view = mActivityLayout.getChildAt(i);
-            if (view instanceof LinearLayout){
+            LinearLayout content = (LinearLayout) mContentView;
+            View rowView = content.getChildAt(i);
+            if (rowView instanceof LinearLayout){
 
-                LinearLayout subjectRow = (LinearLayout) view;
-                EditText subjectInput = (EditText) subjectRow.getChildAt(0);
-                RadioGroup subjectLevelRadioGroup = (RadioGroup) subjectRow.getChildAt(1);
-                RadioButton checkedLevelButton = (RadioButton) findViewById(subjectLevelRadioGroup.getCheckedRadioButtonId());
-
-                String subjectName = subjectInput.getText().toString();
-                String level = checkedLevelButton.getText().toString();
+                LinearLayout subjectRowView = (LinearLayout) rowView;
+                final EditText subjectEditText = (EditText) subjectRowView.getChildAt(0);
+                RadioGroup radioGroup = (RadioGroup) subjectRowView.getChildAt(1);
+                RadioButton checkedRadioButton = (RadioButton) findViewById(radioGroup.getCheckedRadioButtonId());
                 int group = i+1;
-
-                IBSubject subject = new IBSubject(group, subjectName, level);
-                subjectsArray[i] = subject;
+                String subjectName = subjectEditText.getText().toString();
+                String level = checkedRadioButton.getText().toString();
+                if (subjectName.isEmpty() || subjectName.length() < 2){
+                    try {
+                        throw new Exception("Subject Name cannot be empty");
+                    } catch (Exception e) {
+                        Snackbar.make(mContentView, "Subject Name cannot be empty", Snackbar.LENGTH_LONG).show();
+                    }
+                } else {
+                    IBSubject newSubject = new IBSubject(group, subjectName, level);
+                    subjectsArray.add(newSubject);
+                    Snackbar.make(mContentView, "Successfully Gathered Data from UI", Snackbar.LENGTH_LONG).show();
+                }
 
             }
         }
-        Log.d("Getting Data from UI", "Successfully Gathered Data from UI");
+
         return subjectsArray;
 
     }
